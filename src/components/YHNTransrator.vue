f1476470e5bf4503b2e27836880e456e455b5b44
windyakin <windyakin@gmail.com>
2023-07-28 08:16:30 +0900

    Fix path 2

--- src/components/YHNTransrator.vue
+++ src/components/YHNTransrator.vue
@@ -129,7 +129,7 @@ export default defineComponent({
     }
 
     onMounted(async () => {
-      spriteImageObj = await loadImage('/characters.png');
+      spriteImageObj = await loadImage('./characters.png');
       
       if (!container.value) {
         return;
